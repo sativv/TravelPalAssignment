e29a289190b9c085aab7cc6956d6a5e65f1f9564
David Vitas <111963497+sativv@users.noreply.github.com>
2023-10-23 11:23:48 +0200

    Added RegisterWindow

--- TravelPal/MainWindow.xaml.cs
+++ TravelPal/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using TravelPal.Pages;
 
 namespace TravelPal
 {
@@ -23,6 +11,13 @@ namespace TravelPal
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow registerwindow = new RegisterWindow();
+            registerwindow.Show();
+            Close();
         }
     }
 }
---
+++ TravelPal/Pages/RegisterWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace TravelPal.Pages
+{
+    /// <summary>
+    /// Interaction logic for RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+
+            // fill Combobox with Both Enums PopulateComboBox()
+            PopulateComboBox();
+        }
+
+        private void PopulateComboBox()
+        {
+            // fill Combobox with Both Enums PopulateComboBox()
+
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            // Read textboxes
+            // create User and add to UserManager
+
+            // warning if username already exists
+            if (txtPassword.Password == "" || txtUsername.Text == "")
+            {
+                MessageBox.Show("Please fill all fields", "Warning!");
+            }
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
